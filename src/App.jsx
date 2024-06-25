58cdf2c3a0be4bb5350e3a57c125d7c5d3bac799
IbnuAjiWicaksono <153110902+IbnuAjiWicaksono@users.noreply.github.com>
2024-06-25 18:24:04 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import First from './pages/First';
 import Forgot from './pages/Forgot';
 import Dashboard from './pages/Dashboard';
 import Twitter from "./pages/Twitter";
+import Facebook from "./pages/Facebook"
+import Instagram from './pages/Instagram';
+import Youtube from './pages/Youtube';
+import DigitalNews from './pages/DigitalNews';
+import Marketplace from './pages/Marketplace';
 
 const App = () => {
   return (
@@ -14,6 +19,11 @@ const App = () => {
         <Route path="/Forgot" element={<Forgot/>} />
         <Route path='/Dashboard' element={<Dashboard/>}/>
         <Route path='/Twitter' element={<Twitter/>}/>
+        <Route path="/Facebook" element={<Facebook/>}/>
+        <Route path='/Instagram' element={<Instagram/>}/>
+        <Route path='/Youtube' element={<Youtube/>}/>
+        <Route path='/DigitalNews' element={<DigitalNews/>}/>
+        <Route path='/Marketplace' element={<Marketplace/>}/>
       </Routes>
     </BrowserRouter>
     </>
